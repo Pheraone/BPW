2a09c1268bb206f6cab545d0523eac097cb4e4b4
Pheraone <brechje@me.com>
2020-03-05 12:27:35 +0100

    First Commit

---
+++ Assets/Scripts/PlayerView.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerView : MonoBehaviour
+{
+    [SerializeField] private string mouseXInput, mouseYInput;
+    [SerializeField] private float mouseSensitivity;
+
+    [SerializeField] private Transform playerBody;
+
+    private float xAxisClamp;
+
+    private void Awake()
+    {
+        LockCursor();
+        xAxisClamp = 0.0f;
+    }
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis(mouseXInput) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(mouseYInput) * mouseSensitivity * Time.deltaTime;
+
+        xAxisClamp += mouseY;
+
+        if(xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampAxisRotationToValue(270.0f);
+        }   
+        else if (xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampAxisRotationToValue(90.0f);
+        }
+
+        transform.Rotate(Vector3.left* mouseY);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+
+    private void ClampAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+
+
+    }
+}
