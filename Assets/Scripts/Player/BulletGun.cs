f94c4fe0eb721d32e53f7a580a8efb62ca769ada
Pheraone <brechje@me.com>
2020-07-01 23:27:23 +0200

    Yay another working version added the platformgun (projectiles) and the bulletgun (raycast) now switching with different scripts. Do need to adjust the platform gun.

--- Assets/Scripts/Player/BulletGun.cs
+++ Assets/Scripts/Player/BulletGun.cs
@@ -5,14 +5,39 @@ using UnityEngine;
 public class BulletGun : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Camera cam;
+    public float damage;
+    public float range;
+    public float impactForce;
+    public float fireRate;
+    private float nextFire;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Shoot();
+        }
     }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+        {
+            targetHealth target = hit.transform.GetComponent<targetHealth>();
+
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            } 
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal* impactForce);
+            }
+        }
+    }
+
 }
---
+++ Assets/Scripts/Player/targetHealth.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetHealth : MonoBehaviour
+{
+  public void TakeDamage(float amount)
+    {
+        Debug.Log("Damage taken" + amount);
+    }
+}
