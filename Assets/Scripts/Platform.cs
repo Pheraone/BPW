50aed2b22731477cd97e0d41c980b5cc39f28e16
Pheraone <brechje@me.com>
2020-07-03 17:46:01 +0200

    Lifetime added with delete

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -8,11 +8,14 @@ public class Platform : MonoBehaviour
     private Rigidbody platformRB;
     public bool frozen = false;
     [SerializeField] GameObject Player;
-  
-     void Start()
+    public float lifeTime = 5f;
+    private float lifeTimer;
+
+    void Start()
     {
         platformRB = this.gameObject.GetComponent<Rigidbody>();
         gun = GameObject.Find("PlatformGun").GetComponent<PlatformGun>();
+        lifeTimer = lifeTime;
         
     }
 
@@ -27,6 +30,17 @@ public class Platform : MonoBehaviour
         {
             platformRB.gameObject.layer = LayerMask.NameToLayer("Bullet");
             Debug.Log("frozen is "+ frozen);
+        }
+
+        if (frozen == true)
+        {
+            lifeTimer -= Time.deltaTime;
+
+            if (lifeTimer <= 0f)
+            {
+                Debug.Log("I am doing something " + lifeTime);
+                Destroy(gameObject);
+            }
         }
     }
 
